2ae24906200a98b1fc70abd6c5c10cd2d089dfc8
legend1412 <zhaojianhao_1984@163.com>
2020-02-29 00:26:58 +0800

    change

--- src/main/java/com/badou/Test.scala
+++ src/main/java/com/badou/Test.scala
@@ -5,19 +5,18 @@ import org.apache.flink.streaming.api.scala._
 
 object Test {
   def main(args: Array[String]): Unit = {
-    //val benv = ExecutionEnvironment.getExecutionEnvironment
+    val benv = ExecutionEnvironment.getExecutionEnvironment
     val senv = StreamExecutionEnvironment.getExecutionEnvironment
 
 //    val data_path = "D:\\GitHub\\FlinkDemo\\src\\main\\resources\\orders.txt"
 //    val data = benv.readTextFile(data_path)
-//
-//    data.print()
+//    data.map(x => (x.split(",")(1), 1L)).groupBy(0).sum(1).print()
 
-    //benv.execute("test")
+        val data = senv.socketTextStream("192.168.137.3",9999,'\n')
+        val wordcounts = data.map((_,1)).keyBy(0).sum(1)
+        wordcounts.print()
 
-    val data = senv.socketTextStream("192.168.137.3",9999,'\n')
-    val wordcounts = data.map((_,1)).keyBy(0).sum(1)
-    wordcounts.print()
-    senv.execute("test")
+
+    //    senv.execute("test")
   }
 }
