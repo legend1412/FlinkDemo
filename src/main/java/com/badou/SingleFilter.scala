c0bf01cdea94e21ee18c998d7a826abb339a90c4
legend1412 <zhaojianhao_1984@163.com>
2020-03-05 17:38:15 +0800

    adduserid

--- src/main/java/com/badou/SingleFilter.scala
+++ src/main/java/com/badou/SingleFilter.scala
@@ -21,39 +21,75 @@ object SingleFilter {
     //    val aisle_id = jsonObejct("aisle_id")
     //    println(aisle_id)
 
-//    val a = ("a", 1, "3")
-//    val b = ("a", 1, "5")
-//    var tmp1 = mutable.Map[String, Map[String, Int]]() //acc
-//    var tmp2 = mutable.Map[String, Map[String, Int]]()
-//    tmp1 += (a._3 -> Map(a._1 -> a._2))
-//    tmp2 += (b._3 -> Map(b._1 -> b._2))
+    //    val a = ("a", 1, "3")
+    //    val b = ("a", 1, "5")
+    //    var tmp1 = mutable.Map[String, Map[String, Int]]() //acc
+    //    var tmp2 = mutable.Map[String, Map[String, Int]]()
+    //    tmp1 += (a._3 -> Map(a._1 -> a._2))
+    //    tmp2 += (b._3 -> Map(b._1 -> b._2))
 
 
-    val a = ("user_id", Map[String,String]("aisle_id"->"15","department_id"->"23"))
-    val b = ("user_id", Map[String,String]("aisle_id"->"14","department_id"->"23"))
-    var tmp1 = mutable.Map[String, mutable.Map[String, Int]]() //acc
-    println(tmp1)
+    val a = ("user_id", Map[String, String]("aisle_id" -> "15", "department_id" -> "23"))
+    val b = ("user_id", Map[String, String]("aisle_id" -> "14", "department_id" -> "23"))
 
-    var tmp2 = mutable.Map[String, mutable.Map[String, Int]]()
+    //add============
+    val tmp1 = mutable.Map[String, mutable.Map[String, Int]]() //acc
+    println("start(tmp1):" + tmp1) //Map()
 
-    tmp1.getOrElseUpdate("aisle_id",mutable.Map[String,Int]())
-    tmp1.getOrElseUpdate("department_id",mutable.Map[String,Int]())
 
-    a._2.map{attribute=> //aisle_id,department_id (aisle,10)
+    println("add=================================")
+    //因为tmp1是一个{string:map/dict}给对应map进行初始化
+    tmp1.getOrElseUpdate("aisle_id", mutable.Map[String, Int]())
+    tmp1.getOrElseUpdate("department_id", mutable.Map[String, Int]())
+
+    println("init(tmp1):" + tmp1) //Map(department_id -> Map(), aisle_id -> Map())
+
+    a._2.map { attribute => //aisle_id,department_id (aisle_id,14),(department_id,23)
       val attributeValueCntMap = tmp1(attribute._1)
-      attributeValueCntMap+=(attribute._2->(attributeValueCntMap.getOrElse(attribute._2,0)+1))
+      attributeValueCntMap += (attribute._2 -> (attributeValueCntMap.getOrElse(attribute._2, 0) + 1))
     }
 
-    b._2.map{attribute=> //aisle_id,department_id (aisle,10)
-      val attributeValueCntMap = tmp1(attribute._1)
-      attributeValueCntMap+=(attribute._2->(attributeValueCntMap.getOrElse(attribute._2,0)+1))
-    }
-//    tmp1 += (a._1 ->)
-//    tmp2 += (b._1 -> Map(b._1 -> b._2))
+    //    b._2.map { attribute => //aisle_id,department_id (aisle,10)
+    //      val attributeValueCntMap = tmp1(attribute._1)
+    //      attributeValueCntMap += (attribute._2 -> (attributeValueCntMap.getOrElse(attribute._2, 0) + 1))
+    //    }
+    //    tmp1 += (a._1 ->)
+    //    tmp2 += (b._1 -> Map(b._1 -> b._2))
 
-    println(tmp1)
-//    println(tmp2)
-//    val tmp3 = tmp1++tmp2
-//    println(tmp3)
+    println("output(add):" + tmp1) //Map(department_id -> Map(23 -> 2), aisle_id -> Map(15 -> 1, 14 -> 1))
+    //    println(tmp2)
+    //    val tmp3 = tmp1++tmp2
+    //    println(tmp3)
+
+    //merge
+    println("merge==========================")
+    val tmp2 = mutable.Map[String, mutable.Map[String, Int]]()
+
+    tmp2.getOrElseUpdate("aisle_id", mutable.Map[String, Int]())
+    tmp2.getOrElseUpdate("department_id", mutable.Map[String, Int]())
+    println("init(tmp2):" + tmp2)
+
+    //下面三行放开，与注释后是：*********的那行行放开，与现有结果一样???
+    //两个数据流进行merge，相同的key合并，应该是进行更新操作，一个有，一个没有的，进行的插入操作，
+    //    b._2.map { attribute => //aisle_id,department_id (aisle,10)
+    //      val attributeValueCntMap = tmp1(attribute._1)
+    //      attributeValueCntMap += (attribute._2 -> (attributeValueCntMap.getOrElse(attribute._2, 0) + 1))
+    //    }
+
+    b._2.map { attribute => //aisle_id,department_id (aisle,10)
+      val attributeValueCntMap = tmp2(attribute._1)
+      attributeValueCntMap += (attribute._2 -> (attributeValueCntMap.getOrElse(attribute._2, 0) + 1))
+    }
+
+    println("output(tmp2):" + tmp2)
+    tmp1.map { attributeAndValueCnt =>
+      val tmp2ValueCntMap = tmp2(attributeAndValueCnt._1)
+      //(23,2)或者(15,1), (14,1)
+      tmp2ValueCntMap.map { valueCnt =>
+       // valueCnt._1->(attributeAndValueCnt._2.getOrElse(valueCnt._1, 0) + valueCnt._2) *********
+        attributeAndValueCnt._2 += (valueCnt._1 -> (attributeAndValueCnt._2.getOrElse(valueCnt._1, 0) + valueCnt._2))
+      }
+    }
+    println("output(merge)" + tmp1)
   }
 }
--- src/main/java/com/badou/user_prefer/UserPrefer.scala
+++ src/main/java/com/badou/user_prefer/UserPrefer.scala
@@ -11,38 +11,139 @@ import scala.collection.mutable
 object UserPrefer {
   def main(args: Array[String]): Unit = {
     val senv = StreamExecutionEnvironment.getExecutionEnvironment
-    val data = senv.socketTextStream("192.168.110.110",9999)
+    val data = senv.socketTextStream("192.168.110.110", 9999)
+      .setParallelism(4)//并行度
 
+    //mysql中product的数据，放到map中
     val productMap = ReadMysql.productMap
-    // user_id,product_id (orders.join(priors))
-    data.map(_.split(",")).map {
+
+    // user_id,product_id (orders.join(priors)) 数据解析和关联(mysql product属性)
+    val dataParse = data.map(_.split(",")).map {
       x =>
-        val map = JSON.parseObject(productMap(x(1))).asScala.toMap.mapValues(_.toString)
+        val map = JSON.parseObject(productMap(x(1))).asScala.toMap.filter(_._1 != "product_name").mapValues(_.toString)
         (x(0), map)
       //}.print() //7> (189408,Map(department_id -> 16, aisle_id -> 120,
       // product_name -> Icelandic Style Skyr Blueberry Non-fat Yogurt))
-    }.keyBy(_._1)
+    }
+    dataParse.print()
+
+    dataParse
+      .keyBy(_._1)
       .timeWindow(Time.seconds(2))
-    //统计{aisle_id:{aisle_id_val:cnt},department_id:{department_id_val:cnt}}
-//      .aggregate(new UserProductPreferAggregate)
-//      .print()
+      //统计{aisle_id:{aisle_id_val:cnt},department_id:{department_id_val:cnt}} 聚合
+      .aggregate(new UserProductPreferAggregate)
+      //.print()
+      .addSink()
+
 
 
     senv.execute("User Prefer")
   }
-//  class UserProductPreferAggregate
-//    extends AggregateFunction[(String,Map[String,String]),mutable.Map[String,mutable.Map[String,Int]],mutable.Map[String,mutable.Map[String,Int]]]{
-//
-//    override def createAccumulator(): mutable.Map[String, mutable.Map[String, Int]] = {
-//      mutable.Map[String,mutable.Map[String,Int]]
-//    }
-//
-//    override def add(value: (String, Map[String, String]), accumulator: mutable.Map[String, mutable.Map[String, Int]]): mutable.Map[String, mutable.Map[String, Int]] = {
-//
-//    }
-//
-//    override def getResult(accumulator: mutable.Map[String, mutable.Map[String, Int]]): mutable.Map[String, mutable.Map[String, Int]] = ???
-//
-//    override def merge(a: mutable.Map[String, mutable.Map[String, Int]], b: mutable.Map[String, mutable.Map[String, Int]]): mutable.Map[String, mutable.Map[String, Int]] = ???
-//  }
+
+  //不含userid
+  //    2> Map(department_id -> Map(7 -> 2, 19 -> 1), aisle_id -> Map(77 -> 1, 115 -> 1, 117 -> 1))
+  //    8> Map(department_id -> Map(4 -> 1, 16 -> 2), aisle_id -> Map(86 -> 1, 24 -> 1, 91 -> 1))
+  //    4> Map(department_id -> Map(4 -> 4, 16 -> 3, 19 -> 1), aisle_id -> Map(24 -> 1, 83 -> 1, 50 -> 1, 91 -> 1, 120 -> 2, 123 -> 2))
+  //    4> Map(department_id -> Map(12 -> 1, 7 -> 1, 4 -> 1, 16 -> 1, 13 -> 1), aisle_id -> Map(24 -> 1, 89 -> 1, 106 -> 1, 31 -> 1, 120 -> 1))
+
+  class ProductPreferAggregate
+    extends AggregateFunction[(String, Map[String, String]), mutable.Map[String, mutable.Map[String, Int]],
+      mutable.Map[String, mutable.Map[String, Int]]] {
+
+    override def createAccumulator(): mutable.Map[String, mutable.Map[String, Int]] = {
+      mutable.Map[String, mutable.Map[String, Int]]()
+    }
+
+    override def add(value: (String, Map[String, String]), accumulator: mutable.Map[String, mutable.Map[String, Int]]):
+    mutable.Map[String, mutable.Map[String, Int]] = {
+      accumulator.getOrElseUpdate("aisle_id", mutable.Map[String, Int]())
+      accumulator.getOrElseUpdate("department_id", mutable.Map[String, Int]())
+      //Map(department_id->Map(),aisle_id->Map())
+
+      value._2.map { attribute => //aisle_id,department_id (aisle_id,14),(department_id,23)
+        val attributeValueCntMap = accumulator.get(attribute._1).get //accumulator(attribute._1)
+        attributeValueCntMap += (attribute._2 -> (attributeValueCntMap.getOrElse(attribute._2, 0) + 1))
+      }
+      accumulator
+    }
+
+    override def getResult(accumulator: mutable.Map[String, mutable.Map[String, Int]]):
+    mutable.Map[String, mutable.Map[String, Int]] = {
+      accumulator
+    }
+
+    override def merge(a: mutable.Map[String, mutable.Map[String, Int]],
+                       b: mutable.Map[String, mutable.Map[String, Int]]):
+    mutable.Map[String, mutable.Map[String, Int]] = {
+      //具体数据流进来不知道哪个是里面有数据，哪个没有数据，对a，b分别进行初始化
+      a.getOrElseUpdate("aisle_id", mutable.Map[String, Int]())
+      a.getOrElseUpdate("department_id", mutable.Map[String, Int]())
+
+      b.getOrElseUpdate("aisle_id", mutable.Map[String, Int]())
+      b.getOrElseUpdate("department_id", mutable.Map[String, Int]())
+
+      //主要合并逻辑，将b中的统计数据（对b中的数据进行遍历）和里面的值进行更新，然后将更新后的值再更新到a中
+      a.map { attributeAndValueCnt =>
+        val bValueCntMap = b(attributeAndValueCnt._1)
+        bValueCntMap.map { valueCnt =>
+          attributeAndValueCnt._2 += (valueCnt._1 -> (attributeAndValueCnt._2.getOrElse(valueCnt._1, 0) + valueCnt._2))
+        }
+      }
+      a
+    }
+  }
+
+  //增加user_id
+  //  2> (99623,Map(department_id -> Map(7 -> 2, 19 -> 1), aisle_id -> Map(77 -> 1, 115 -> 1, 117 -> 1)))
+  //  4> (189408,Map(department_id -> Map(4 -> 4, 16 -> 3, 19 -> 1), aisle_id -> Map(24 -> 1, 83 -> 1, 50 -> 1, 91 -> 1, 120 -> 2, 123 -> 2)))
+  //  4> (19376,Map(department_id -> Map(12 -> 1, 7 -> 1, 4 -> 1, 16 -> 1, 13 -> 1), aisle_id -> Map(24 -> 1, 89 -> 1, 106 -> 1, 120 -> 1, 31 -> 1)))
+  //  8> (111036,Map(department_id -> Map(4 -> 1, 16 -> 2), aisle_id -> Map(24 -> 1, 86 -> 1, 91 -> 1)))
+  class UserProductPreferAggregate
+    extends AggregateFunction[(String, Map[String, String]), (String, mutable.Map[String, mutable.Map[String, Int]]),
+      (String, mutable.Map[String, mutable.Map[String, Int]])] {
+
+    override def createAccumulator(): (String, mutable.Map[String, mutable.Map[String, Int]]) = {
+      ("", mutable.Map[String, mutable.Map[String, Int]]())
+    }
+
+    override def add(value: (String, Map[String, String]),
+                     accumulator: (String, mutable.Map[String, mutable.Map[String, Int]])):
+    (String, mutable.Map[String, mutable.Map[String, Int]]) = {
+      accumulator._2.getOrElseUpdate("aisle_id", mutable.Map[String, Int]())
+      accumulator._2.getOrElseUpdate("department_id", mutable.Map[String, Int]())
+      //Map(department_id->Map(),aisle_id->Map())
+
+      value._2.map { attribute => //aisle_id,department_id (aisle_id,14),(department_id,23)
+        val attributeValueCntMap = accumulator._2.get(attribute._1).get //accumulator(attribute._1)
+        attributeValueCntMap += (attribute._2 -> (attributeValueCntMap.getOrElse(attribute._2, 0) + 1))
+      }
+      (value._1, accumulator._2)
+    }
+
+    override def getResult(accumulator: (String, mutable.Map[String, mutable.Map[String, Int]])):
+    (String, mutable.Map[String, mutable.Map[String, Int]]) = {
+      accumulator
+    }
+
+    override def merge(a: (String, mutable.Map[String, mutable.Map[String, Int]]),
+                       b: (String, mutable.Map[String, mutable.Map[String, Int]])):
+    (String, mutable.Map[String, mutable.Map[String, Int]]) = {
+      //具体数据流进来不知道哪个是里面有数据，哪个没有数据，对a，b分别进行初始化
+      a._2.getOrElseUpdate("aisle_id", mutable.Map[String, Int]())
+      a._2.getOrElseUpdate("department_id", mutable.Map[String, Int]())
+
+      b._2.getOrElseUpdate("aisle_id", mutable.Map[String, Int]())
+      b._2.getOrElseUpdate("department_id", mutable.Map[String, Int]())
+
+      //主要合并逻辑，将b中的统计数据（对b中的数据进行遍历）和里面的值进行更新，然后将更新后的值再更新到a中
+      a._2.map { attributeAndValueCnt =>
+        val bValueCntMap = b._2(attributeAndValueCnt._1)
+        bValueCntMap.map { valueCnt =>
+          attributeAndValueCnt._2 += (valueCnt._1 -> (attributeAndValueCnt._2.getOrElse(valueCnt._1, 0) + valueCnt._2))
+        }
+      }
+      a
+    }
+  }
+
 }
